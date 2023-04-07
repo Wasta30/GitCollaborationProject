0b710d4d63b715bb3d7c4f024dcbb50eecc2f63e
khayalabva <khayalabva@gmail.com>
2023-04-07 22:52:06 +0200

    Runner classes updated

--- src/test/java/com/practice/runners/FailedTestRunner.java
+++ src/test/java/com/practice/runners/FailedTestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        glue = "com/cydeo/step_definitions",
+        glue = "com/practice/runners",
         features = "@target/rerun.txt"
 )
 public class FailedTestRunner {
